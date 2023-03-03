6cd2a4312c131704e437325b44425067e969e9c0
Gabriel Gomes de Almeida <gabrielg299@gmail.com>
2023-03-03 11:39:44 -0300

    Update adicionado

--- src/main/java/com/todolist/todo/controllers/ToDoController.java
+++ src/main/java/com/todolist/todo/controllers/ToDoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,4 +63,15 @@ public class ToDoController {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateToDo(@PathVariable(value = "id") int id, @RequestBody @Valid ToDoDTO obj){
+        Optional<ToDo> toDoOpt = toDoService.findById(id);
+        if (!toDoOpt.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não encontrado");
+        }
+        var toDo = new ToDo();
+        BeanUtils.copyProperties(obj, toDo);
+        toDo.setId(toDoOpt.get().getId());
+        return ResponseEntity.status(HttpStatus.OK).body(toDoService.save(toDo));
+    }
 }
